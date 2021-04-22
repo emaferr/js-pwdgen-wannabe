d7f62ab5140b7b25416b6474c34f91bb00cbf2aa
ema.ferr <78190534+emaferr@users.noreply.github.com>
2021-04-22 17:03:56 +0200

    add user interactions add related reply message

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,30 @@
 // asks the user for his name
+var userName = prompt("What is your name?");
+console.log();
 
 // asks the user for his surname
+var userSurname = prompt("What's your last name?");
+console.log();
 
 // asks the user for their favorite color
+var userFavoriteColor = prompt("What is your favorite color?");
+console.log();
+
+// define variable to be added to the result
+var numberAdd = "21";
+console.log();
+
+// identify the greeting variable
+var greating = "Hello " + userName;
+console.log();
+
+// identify the variable result
+var passwordGenerate = userName + userSurname + userFavoriteColor + numberAdd;
+console.log();
 
 // write the result to the user
+document.getElementById("greeting_user").innerHTML = greating.toUpperCase();
+
+document.getElementById("generate_password").innerHTML = "Your generated password is " +  passwordGenerate.bold().toLowerCase();
+
+
